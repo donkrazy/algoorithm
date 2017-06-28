10bca49d648c99c06136ef9e7c06dfe6af0ff34a
donkrazy <donkrazy12@gmail.com>
2017-06-28 11:51:14 +0000

    Timeout
    Using itertools: generate all possible partition
    idea from CODE6.2

--- 6.3-PICNIC.py
+++ 6.3-PICNIC.py
@@ -1,20 +1,26 @@
-import itertools
+from itertools import combinations
+from math import factorial
 
 
-def make_pair(n):
-    ret = []
-    perm_list = itertools.permutations(range(n))
-    for perm in perm_list:
-        perm = list(zip(perm[0::2], perm[1::2]))
-        ret.append(perm)
-    return ret
+def make_combinations(topick, picked, ret):
+    if len(topick) == 0:
+        ret.append(picked)
+    for duo in combinations(topick, 2):
+        topick_copy = topick.copy()
+        picked_copy = picked.copy()
+        picked_copy.append(duo)
+        
+        for item in duo:
+            topick_copy.remove(item)
+        make_combinations(topick_copy, picked_copy, ret)
 
 
 c = int(input())
 for prob in range(c):
     # generate dataset
     n, m = (int(i) for i in input().split())
-    pairs_available = make_pair(n)
+    pairs_available = []
+    make_combinations(list(range(n)), [], pairs_available)
     data = [int(i) for i in input().split()]
     table = list(zip(data[0::2], data[1::2]))
 
@@ -28,7 +34,6 @@ for prob in range(c):
                 break
 
         if flag:
-            print(pair_list)
             count += 1
 
-    print(count)
+    print(int(count / factorial(n/2)))
