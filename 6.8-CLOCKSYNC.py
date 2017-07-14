4c6cb99870c95da56bd43192de17987bff87a9c0
donkrazy <donkrazy12@gmail.com>
2017-07-14 15:20:31 +0900

    Timeout
    미리 4^10개 리스트 만들어 놓고 하기... check()에서 시간이 많이 걸리는 것 같다..

--- 6.8-CLOCKSYNC.py
+++ 6.8-CLOCKSYNC.py
@@ -1,5 +1,3 @@
-import numpy
-
 switch_info = '''
 0   0, 1, 2
 1   3, 7, 9, 11
@@ -12,46 +10,52 @@ switch_info = '''
 8   1, 2, 3, 4, 5
 9   3, 4, 5, 9, 13
 '''
-switch_list = []
+switch_dict = {}
 for item in switch_info.split('\n')[1:-1]:
     idx_list = [int(i) for i in item[1:].split(',')]
     available = [1 if j in idx_list else 0 for j in range(16)]
-    switch_list.append(available)
+    switch_dict[int(item[0])] = available
 
 
-def check(picked_list):
+def check(picked_list, init_condition):
     # y = ax + b
-    # switch_list = [[0000111122223333], [1111222233330000], ... ]    # 10 by 16
-    # picked_list = [1,1,1,1,1,1,1,1,1,1,1,1]   # 10 by 1
-    # inner_product = switch_list (dot) picked_list   # 16 by 1
-    # y = inner_product + init_condition  # 16 by 1
-    inner_product = numpy.dot(picked_list, switch_list)
-    y = inner_product + init_condition
+    y = []
+    y.append(init_condition)
+    for i in range(len(picked_list)):
+        k = picked_list[i]
+        ll = [k * j for j in switch_dict[i]]
+        y.append(ll)
 
-    if all(v == 0 for v in y % 4):
+    y = [sum(i) % 4 for i in zip(*y)]
+    if all(v == 0 for v in y):
         ans.append(sum(picked_list))
         return True
     return False
 
 
-def traverse(picked_list):
-    has_found = False
+def generate_list(picked_list=None):
+    if picked_list is None:
+        picked_list = []
+
     if len(picked_list) == 10:
-        has_found = check(picked_list)
-        return has_found
+        available_list.append(list(picked_list))
+        return
 
     for i in range(4):
         picked_list.append(i)
-        has_found |= traverse(picked_list)
+        generate_list(picked_list)
         picked_list.pop()
-    return has_found
+
+
+available_list = []
+generate_list()
 
 
 num_prob = int(input())
 for prob in range(num_prob):
     ans = []
     init_condition = [int(i) / 3 for i in input().split()]
-    if traverse([]):
-        print(min(ans))
-    else:
-        print(-1)
+    for item in available_list:
+        if check(item, init_condition):
+            ans.append(sum(item))
+    print(min(ans) if len(ans) != 0 else -1)
