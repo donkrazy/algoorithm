ca26f817031d4f8eecd2b8c0e0c223bc9077f0f7
donkrazy <donkrazy12@gmail.com>
2017-07-30 18:09:16 +0900

    Undone
    like bubble sort...

--- 7.4-FENCE.py
+++ 7.4-FENCE.py
@@ -1,17 +1,31 @@
-def draw_line(height_list, y):
-    return [True if height >= y else False for height in height_list]
+def narashi(height_list):
+    changed = False
+    for i in range(len(height_list) - 1):
+        left = height_list[i]
+        right = height_list[i + 1]
+        if left == 0 or right == 0 or left == right:
+            continue
+        # left != right
+        changed = True
+        # clean code
+        if left > right:  
+            area_c = right * 2
+            if left > area_c:
+                height_list[i + 1] = 0
+            else:
+                height_list[i] = right
+        else:
+            area_c = left * 2
+            if right > area_c:
+                height_list[i] = 0
+            else:
+                height_list[i + 1] = left
+    return changed
 
-def count_true(TF_list):
-    count_list = []
-    count = 0
-    for TF in TF_list:
-        if TF:
-            count += 1
-        elif not TF and count != 0:
-            count_list.append(count)
-            count = 0
-    count_list.append(count)  # all True
-    return max(count_list)
+
+def get_area(height_list):
+    area_list = []
+    return max(area_list)
 
 
 if __name__ == '__main__':
@@ -19,10 +33,11 @@ if __name__ == '__main__':
     for prob in range(num_prob):
         n = int(input())
         height_list = [int(i) for i in input().split()]
-        max_y = max(height_list)
-        area_list = []
-        for y in range(max_y, 0, -1):
-            TF_list = draw_line(height_list, y)
-            max_width = count_true(TF_list)
-            area_list.append(y * max_width)
-        print(max(area_list))
+        while True:
+            has_changed = narashi(height_list)
+            if not has_changed:
+                break
+        # max_area = get_area(height_list)
+        # print(max_area)
+        print(height_list)
+        
