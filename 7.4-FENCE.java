17919c5b18895f59b44d05680aa3f1859289a5b5
donkrazy <donkrazy12@gmail.com>
2017-07-31 21:34:03 +0900

    Success
    divide-conquer

--- 7.4-FENCE.java
+++ 7.4-FENCE.java
@@ -1,41 +1,55 @@
 import java.util.Arrays;
 import java.util.Scanner;
-import java.lang.Math;
 
 public class Fence {
-	private static int findMaxArea(String[] line) {
-		int[] heights = Arrays.stream(line).mapToInt(Integer::parseInt).toArray();
-		int highest = Arrays.stream(heights).max().getAsInt();
-		int maxArea = 0;
-		for (int h = highest; h > 0; h--) {
-			int area = getAreaUnder(heights, h);
-			maxArea = Math.max(maxArea, area);
-		}
-		return maxArea;
-	}
-
-	private static int getAreaUnder(int[] heights, int givenHeight) {
-		int count = 0;
-		int maxWidth = 0;
-		for (int h : heights) {
-			if (h >= givenHeight) {
-				count++;
-			} else {
-				maxWidth = Math.max(maxWidth, count);
-				count = 0;
-			}
-		}
-		return maxWidth * givenHeight;
-	}
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int num_prob = Integer.parseInt(scanner.nextLine());
 		while (num_prob-- > 0) {
 			int n = Integer.parseInt(scanner.nextLine());
 			String[] line = scanner.nextLine().split(" ");
-			System.out.println(findMaxArea(line));
+			int[] heights = Arrays.stream(line).mapToInt(Integer::parseInt).toArray();
+			System.out.println(findMaxArea(heights, 0, n - 1));
 		}
 		scanner.close();
+	}
+
+	private static int findMaxArea(int[] heights, int left, int right) {
+		// 1) base case
+		if (left == right) {
+			return heights[left];
+		}
+
+		// 2) left or right max case
+		int mid = (left + right) / 2;
+		int leftMax = findMaxArea(heights, left, mid);
+		int rightMax = findMaxArea(heights, mid + 1, right);
+		int ret = Math.max(leftMax, rightMax);
+
+		// 3) center overlapping case
+		// start point: rectangle with (mid, 0), (mid+1, heights[mid+1])
+		int i = mid;
+		int j = mid + 1;
+		int h = Math.min(heights[i], heights[j]);
+		ret = Math.max(ret, 2 * h);
+		// expand to left or right
+		while (true) {
+			if (i == left && j == right) {
+				break;
+			}
+			if (j == right) { // to left
+				h = Math.min(h, heights[--i]);
+			} else if (i == left) { // to right
+				h = Math.min(h, heights[++j]);
+			} else if (heights[i - 1] >= heights[j + 1]) { // to left
+				h = Math.min(h, heights[--i]);
+			} else if (heights[i - 1] < heights[j + 1]) { // to right
+				h = Math.min(h, heights[++j]);
+			} else { // catch error
+				throw new RuntimeException();
+			}
+			ret = Math.max(ret, (j - i + 1) * h);
+		}
+		return ret;
 	}
 }
