73eadd4d6abf6776ea629f6e7fcbbd8a3ea9d88a
donkrazy <donkrazy12@gmail.com>
2017-08-03 22:00:06 +0900

    Runtime error
    karatsuba

--- 7.6-FANMEETING.py
+++ 7.6-FANMEETING.py
@@ -1,15 +1,78 @@
-def solve(a, b):
-    # 1) multiply with 'b' reversed, without rounding up
+def multiply(a, b):
     c = [0] * (len(a) + len(b) - 1)
-    b.reverse()
     for i in range(len(a)):
         for j in range(len(b)):
-            # c[i + j] += a[i] * b[j]
-            c[i + j] |= a[i] & b[j]  # boolean version
+            c[i + j] += a[i] * b[j]
+    return c
 
-    # 2) count 0 in c except for a partial hug
+# a += b * 10^k
+def add(a, b, k=0):
+    for i in range(k):
+        a.append(0)
+    j = 0
+    for i in b[::-1]:
+        j -= 1
+        a[j] += i
+        
+
+# a -= b
+def subtract(a, b):
+    j = 0
+    for i in b[::-1]:
+        j -= 1
+        a[j] -= i
+        
+
+def normalize(a):
+    for i in range(len(a)-1, 1, -1):  # omit prepend
+        if a[i] >= 10:
+            a[i - 1] += int(a[i] / 10)
+            a[i] = a[i] % 10
+        elif a[i] < 0:
+            borrow = int((abs(a[i]) + 9) / 10)
+            a[i - 1] -= borrow
+            a[i] += borrow * 10
+
+
+def karatsuba(a, b):
+    # 1) base cases
+    if len(a) < len(b):
+        return karatsuba(b, a)
+    if len(a) == 0 or len(b) == 0:
+        return []
+    if len(a) <= 50:
+        return multiply(a, b)
+    
+    # 2) split: a = a0 + a1; b = b0 + b1;
+    half = int(len(a))
+    a0 = a[:half]
+    a1 = a[half:]
+    b0 = b[:min(half, len(b))]
+    b1 = b[min(half, len(b)):]
+    
+    # 3) z0 = a0 * b0; z2 = a1 * b1; 
+    #    z1 = (a0 + a1) * (b0 + b1) - z0 - z2;
+    z0 = karatsuba(a0, b0)
+    z2 = karatsuba(a1, b1)
+    add(a0, a1)
+    add(b0, b1)
+    z1 = karatsuba(a0, b0)
+    subtract(z1, z0)
+    subtract(z1, z2)
+    normalize(z1)
+    
+    # 4) a * b = z0 + z1 * 10^n + z2 * 10 ^ 2n
+    add(z0, z1, half)
+    add(z0, z2, 2 * half)
+    normalize(z0)
+    return z0
+    
+
+def solve(a, b):
+    # count 0 in c except for a partial hug
     count = 0
-    for k in c[len(a) - 1:len(c) - len(a) + 1]:
+    c = karatsuba(a, b)
+    for k in c[len(b) - 1:len(c) - len(b) + 1]:
         if k == 0:
             count += 1
     return count
@@ -18,8 +81,6 @@ def solve(a, b):
 if __name__ == '__main__':
     num_prob = int(input())
     for prob in range(num_prob):
-        # member_list = [1 if i == 'M' else 0 for i in input()]
-        # fan_list = [1 if i == 'M' else 0 for i in input()]
-        member_list = [True if i == 'M' else False for i in input()]  # boolean version
-        fan_list = [True if i == 'M' else False for i in input()]  # boolean version
-        print(solve(member_list, fan_list))
+        member_list_reversed = [1 if i == 'M' else 0 for i in input()[::-1]]
+        fan_list = [1 if i == 'M' else 0 for i in input()]
+        print(solve(fan_list, member_list_reversed))
