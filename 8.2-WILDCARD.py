e30dd8f3d63178ff484dab34556645d16fc6cede
donkrazy <donkrazy12@gmail.com>
2017-08-10 23:32:05 +0900

    Success
    brute-force

--- 8.2-WILDCARD.py
+++ 8.2-WILDCARD.py
@@ -2,29 +2,29 @@ def match(wildcard, file):
     pos = 0
     while pos < len(wildcard) and pos < len(file) and (wildcard[pos] == file[pos] or wildcard[pos] == '?'):
         pos += 1
-    if pos == len(wildcard):
-        if pos == len(file):
-            return True
-        else:
-            return False
 
-    if pos == len(file):
-        if wildcard[pos] == '*':
-            return True
+    # papa^ / papa^
+    if pos == len(wildcard) and pos == len(file):
+        return True
+
+    # papa^ / papa^dk
+    if pos == len(wildcard) and pos < len(file):
+        return False
+
+    # pap* / papcvz
+    if pos == len(wildcard) - 1 and wildcard[pos] == '*':
+        return True
 
     if wildcard[pos] == '*':
-        for j in range(pos, len(file) - 1):
+        for j in range(pos, len(file)):
             if match(wildcard[pos + 1:], file[j:]):
                 return True
     return False
 
 
 def solve(wildcard, files):
-    print('input:', wildcard, files)
-    for file in files:
-        if not match(wildcard, file):
-            files.remove(file)
-    for file in sorted(files):
+    ret = [file for file in files if match(wildcard, file)]
+    for file in sorted(ret):
         print(file)
 
 
