fd784efed4464511ae2e0ae53ff0de08d667a577
donkrazy <donkrazy12@gmail.com>
2017-08-14 19:42:32 +0900

    Success
    Using memoization, running time 428ms -> 164ms

--- 8.2-WILDCARD.py
+++ 8.2-WILDCARD.py
@@ -1,31 +1,35 @@
-def match(wildcard, file, i, j):
-    ret = cache(i, j)
+def match(wildcard, file, cache=None, i=0, j=0):  # default value for initialization
+    if cache is None:
+        cache = [[-1] * 101 for _ in range(101)]  # -1: unidentified, 0: not match, 1: match
+
+    if cache[i][j] == 1:
+        return True
+    elif cache[i][j] == 0:
+        return False
+
     while i < len(wildcard) and j < len(file) and (wildcard[i] == file[j] or wildcard[i] == '?'):
         i += 1
         j += 1
 
     # papa^ / papa^
-    if i == len(wildcard) and j == len(file):
-        return True
-
     # papa^ / papa^dk
-    if i == len(wildcard) and j < len(file):
-        return False
+    if i == len(wildcard):
+        return j == len(file)
 
-    # pap* / papcvz
-    if i == len(wildcard) - 1 and wildcard[i] == '*':
-        return True
-
+    # papa* / papadfa
+    # pa*a / pazzzzzza
     if wildcard[i] == '*':
-        for k in range(i, len(file)):
-            if match(wildcard[i + 1:], file[k:]):
+        for k in range(j, len(file) + 1):
+            if match(wildcard, file, cache, i + 1, k):
+                cache[i][j] = 1
                 return True
+
+    cache[i][j] = 0
     return False
 
 
-def solve(wildcard, files, cache):
-    cache = [[-1] * 100 for _ in range(100)]
-    ret = [file for file in files if match(wildcard, file, 0, 0)]
+def solve(wildcard, files):
+    ret = [file for file in files if match(wildcard, file)]
     for file in sorted(ret):
         print(file)
 
