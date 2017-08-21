b9fe778147790ca18884d874925465abcab4c076
donkrazy <donkrazy12@gmail.com>
2017-08-21 19:11:47 +0900

    Timeout

--- 8.7-PI.py
+++ 8.7-PI.py
@@ -1,11 +1,10 @@
 import sys
-sys.setrecursionlimit(3334)
+sys.setrecursionlimit(3334)  # 10000 / 3
 
 
-def difficulty(b):
-    # Assume that 3 <= len(b) <= 5
-    b = [int(i) for i in b]
-
+# check difficulty of a[idx:dix + diff]
+def difficulty(a, idx, diff):
+    b = a[idx:idx + diff]
     # case 1
     if len(set(b)) == 1:
         return 1
@@ -27,7 +26,7 @@ def difficulty(b):
     return 10
 
 
-# find min value from a[idx:]
+# find min difficulty from a[idx:]
 def solve(a, cache, idx):
     if cache[idx] != -1:
         return cache[idx]
@@ -36,11 +35,11 @@ def solve(a, cache, idx):
     if len_a < 3:
         ret = 9999999
     elif len_a >= 3 and len_a <= 5:
-        ret = difficulty(a[idx:])
+        ret = difficulty(a, idx, len_a)
     else:
-        ret = min(difficulty(a[idx:idx + 3]) + solve(a, cache, idx + 3),
-                  difficulty(a[idx:idx + 4]) + solve(a, cache, idx + 4),
-                  difficulty(a[idx:idx + 5]) + solve(a, cache, idx + 5))
+        ret = min(difficulty(a, idx, 3) + solve(a, cache, idx + 3),
+                  difficulty(a, idx, 4) + solve(a, cache, idx + 4),
+                  difficulty(a, idx, 5) + solve(a, cache, idx + 5))
 
     cache[idx] = ret
     return ret
@@ -49,6 +48,6 @@ def solve(a, cache, idx):
 if __name__ == '__main__':
     num_probs = int(input())
     for _ in range(num_probs):
-        a = input()
+        a = [int(i) for i in list(input())]
         cache = [-1] * len(a)
         print(solve(a, cache, 0))
