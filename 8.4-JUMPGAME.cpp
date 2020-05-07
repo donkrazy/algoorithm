1efed6c464c50686dbede98998d75584e1abdbb9
donkrazy <covfefe2@naver.com>
2020-05-07 20:06:45 +0900

    cpp

---
+++ 8.4-JUMPGAME.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int n, board[100][100];
+
+bool jump(int y, int x) {
+    if (y >= n || x >= n) return false;
+    if (y == n-1 && x== n-1) return true;
+    int jumpSize = board[y][x];
+    return jump(y + jumpSize, x) || jump(y, x + jumpSize);
+}
+
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
